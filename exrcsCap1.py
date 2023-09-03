f563ac0258628c50675590924e2c974a33605e8d
miguelm43152 <sweetbloodmigue@gmail.com>
2023-09-03 16:05:04 -0600

    Create exrcsCap1.py

---
+++ exrcsCap1.py
@@ -0,0 +1,9 @@
+#1.-Escribe un mensaje en una variable que puedes llamar como quieras.
+saludo = "Hola, esta es una variable."
+#2.-Crea dos variables y almacena en ellas un número.
+num1 = 5; num2 = 6
+#3.-Intenta (todavía no te he mostrado como) sumar las dos variables del ejercicio anterior y guarda el resultado de esta suma en una tercera variable.
+result = num1 + num2
+#4.-Si has conseguido hacer el ejercicio anterior, imprime el valor de la variable del resultado.
+#En cambio, si no lo has conseguido imprime el texto de la variable del primer ejercicio
+print( f"el resultado de la suma es: {result}")
