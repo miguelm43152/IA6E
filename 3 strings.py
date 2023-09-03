b719781b984ba221d5ca7361b613cb018a960873
miguelm43152 <sweetbloodmigue@gmail.com>
2023-09-03 16:23:52 -0600

    Usar " " y ' ' para strings
    Y también cómo imprimirlos

--- 3 strings.py
+++ 3 strings.py
@@ -1,7 +1,9 @@
 
 #5.-    Escribe dos variables con strings. Uno con comillas simples '' y otro con comillas dobles"".
+
 text1 = "Este es una forma de escribir un string."
 text2 = 'y esta es otra forma de hacerlo....'
+
 #6.-    Almacena literalmente esta frase en una variable e imprímela:
 #    "print()" se utiliza para imprimir valores en la consola.
 #    Si te da error de sintaxis debes revisar el capítulo.
