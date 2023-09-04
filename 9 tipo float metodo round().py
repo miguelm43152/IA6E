a24f14bd87cfe0d7f3458ab7f3ad7f22b9aa50de
miguelm43152 <sweetbloodmigue@gmail.com>
2023-09-03 18:47:40 -0600

    round( float, cifras significativas)

---
+++ 9 tipo float metodo round().py
@@ -0,0 +1,9 @@
+# Miguel Ángel Mendoza Hernández
+# Registro: 20110144
+# Inteligencia Artificial
+# Grupo 6E
+##################### CAPITULO 9: Los floats y el método round() de Python ###########################################
+
+# 22.- Redondea con cinco decimales el número 17.567383292929200234.
+numeroFloat = 17.567383292929200234
+print( "Numero redondeado con 5 cifras: ", round(numeroFloat,5) )
