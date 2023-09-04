2b2294970ace7c3b9e412de2389e9ca5f1bfa2fe
miguelm43152 <sweetbloodmigue@gmail.com>
2023-09-03 18:47:04 -0600

    como usar la operación exponente
    ej. 10 ** 3 = 1000

---
+++ 8 exponentes.py
@@ -0,0 +1,10 @@
+# Miguel Ángel Mendoza Hernández
+# Registro: 20110144
+# Inteligencia Artificial
+# Grupo 6E
+########################### CAPITULO 8: cómo calcular exponentes en python ###########################################
+
+#21.- Utilizando solo el número 2 (cuantas veces necesites) y 
+#     sin utilizar exponentes, debes obtener el resultado 1024.
+
+print( "Sin usar exponentes: ", 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2, '\nUsando exponentes: ', 2 ** 10)
