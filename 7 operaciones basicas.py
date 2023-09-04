e3a92eaa21f9b9f216b82c7d8f8d8fbc7dc8a81c
miguelm43152 <sweetbloodmigue@gmail.com>
2023-09-03 18:47:53 -0600

    + . - , * , /

---
+++ 7 operaciones basicas.py
@@ -0,0 +1,29 @@
+# Miguel Ángel Mendoza Hernández
+# Registro: 20110144
+# Inteligencia Artificial
+# Grupo 6E
+########################### CAPITULO 7: suma, resta, multiplicación y división ###########################################
+
+# En los siguientes ejercicios quiero que vayas obteniendo un poquito de lógica matemática. Es muy importante en programación.
+#
+# 16 Almacena en una variable una suma que de como resultado el número 87. 
+#    Es obligatorio incluir en dicha suma los números 20 y 23.
+suma = 20 + 23 + 44
+# 17 Almacena en una variable una resta que de como resultado el número negativo -87. 
+#    Es obligatorio incluir en dicha resta los números positivos 20 y 23.
+resta = 20 - 23 - 84
+# 18 Almacena en una variable una multiplicación que de como resultado el número 870. 
+#    Es obligatorio incluir en dicha multiplicación los números 20 y 23.
+multiplicacion = 10 * 87
+# 19 Almacena en una variable una división que de como resultado el número 10 
+#    (no hace falta que sea 10 exacto, puede tener decimales). 
+#    Es obligatorio incluir en dicha división los números 5000 y 230.
+"""El resultado es 10.869565217391305"""
+division = 5000 / 230 / 2
+# 20 A la siguiente operación añade los símbolos aritméticos básicos (+, -, *, /).
+#    Debes utilizar los cuatro sin repertirlos. Uno para cada espacio:
+#    operacion = 10 ____ 5 ____ 15 ____ 17
+operacion = 10 / 5 + 15 - 17
+
+print( suma + resta + multiplicacion + division)
+print( "resultados ", suma,resta,multiplicacion,                 division, "\nfin de programa\n\tGracias!!! :)" ) #de esta forma se imprimen las variables uno por uno
