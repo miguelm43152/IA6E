85472ef2b2b6cc1c5258b301c14379b13fc9749d
miguelm43152 <sweetbloodmigue@gmail.com>
2023-09-03 18:48:16 -0600

    creando lista y extrayendo sus elementos

---
+++ 10 crear listas y usarlas.py
@@ -0,0 +1,22 @@
+# Miguel Ángel Mendoza Hernández
+# Registro: 20110144
+# Inteligencia Artificial
+# Grupo 6E
+##################### CAPITULO 10: Cómo crear listas en python y utilizarlas ###########################################
+
+# 23 De la siguiente lista, ¿qué color está en la posición 3?
+#
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+print( '\nEl color ' ,colores[3], " se encuentra en la posición 3\n" )
+# 24 ¿En qué posición se encuentra el color 'rojo'? ¿y el 'rosa'?
+print( f"El color {colores[7]} está en la posición 7 y el color ", colores[0], " se encuentra en la posición 0." )
+# 25 Crea una lista que contenga los siguientes valores en las posiciones indicadas.
+#
+#    "uno" en la posición 4.
+#    "dos" en la posición 1.
+#    "tres" en la posición 0.
+#    "cuatro" en la posición 3.
+#    "cinco" en la posición 2.
+#
+lista = ["tres", "dos", "cinco", "cuatro", "uno"]
+print( "\nImprimiendo el contenido de la lista: ", lista, "\n" )  
