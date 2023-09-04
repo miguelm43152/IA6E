35a0906ebe2e45b27143cb519bc3fc1f47ba402a
miguelm43152 <sweetbloodmigue@gmail.com>
2023-09-03 18:44:45 -0600

    Concatenando
    Concatenamos strings declarados, variables con strings y variables con variables

---
+++ 4 como concatenar.py
@@ -0,0 +1,25 @@
+# Miguel Ángel Mendoza Hernández
+# Registro: 20110144
+# Inteligencia Artificial
+# Grupo 6E
+########################### CAPITULO 4: VARIABLES ###############################################################
+
+#7    Concatena dos strings en una variable. Puedes comprobar el resultado con un print().
+variable = "esta es " + "una sola frase"
+print( variable)
+#8    Concatena dos variables con strings en una tercera variable.
+var1 = "un solo string "
+var2 = ' guardado en una variable'
+var3 = var1 + var2
+#9    Concatena una variable con string y un string directamente en un print().
+v1 = "holaaa "
+print(v1 + "mundo")
+#10    Escribe tu nombre y apellidos separados en variables. Después concatena estas variables dentro de otra. 
+# No te olvides de concatenar los espacios entre el nombre y cada apellido. Finalmente, imprime el resultado.
+miNombre = "Miguel Ángel"
+miApellido = "Mendoza Hernández"
+print( miNombre + ' ' + miApellido )
+#11    Concatena dos números, no los sumes.
+num1 = '50'
+num2 = '60'
+print(num1 + num2)
