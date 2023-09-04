7e875641f8712ef68e88c7331868617019aad4ff
miguelm43152 <sweetbloodmigue@gmail.com>
2023-09-03 18:46:33 -0600

    uso de salto y tabulación
    '\n' para salto de linea
    "\t" para tabulación

---
+++ 6 saltos de linea y tabulacion.py
@@ -0,0 +1,21 @@
+# Miguel Ángel Mendoza Hernández
+# Registro: 20110144
+# Inteligencia Artificial
+# Grupo 6E
+########################### CAPITULO 6: Saltos de linea y tabulación ###########################################
+
+# 15 .-Escribe el contenido (strings) de todos estos print() 
+# en uno solo con un string. Debes utilizar los saltos de línea 
+# para que quede cada lenguaje de programación escrito en una línea. 
+# Al final, añade dos lenguajes más. Además, quiero que añadas un guion 
+# al principio de cada fila y un punto al final. Debería quedarte algo así:
+#
+#    -Python.
+#    -JavaScript.
+#    -Java.
+#    -PHP.
+#    -TypeScript.
+#    -SQL.
+#    -COBOL.
+
+print(  "Lenguajes de programación:\n\t-Python.\n\t\t-JavaScript.\n\t\t\t-Java.\n\t\t\t\t-PHP.\n\t\t\t\t\t-TypeScript.\n\t\t\t\t\t\t-SQL.\n\t\t\t\t\t\t\t-COBOL." )
