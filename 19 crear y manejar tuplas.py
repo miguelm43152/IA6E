ae2a7fd1bf9a5bea51424aa6474b2b0514cd25f1
miguelm43152 <143959244+miguelm43152@users.noreply.github.com>
2023-09-06 09:39:58 -0600

    Como declarar tuplas
    y acceder a cada sus elementos 
    con uso de indices

---
+++ 19 crear y manejar tuplas.py
@@ -0,0 +1,15 @@
+# Miguel Ángel Mendoza Hernández
+# Registro: 20110144
+# Inteligencia Artificial
+# Grupo 6E
+##################### CAPITULO 19: Crear y manejar tuplas ########################################
+
+#33.- Imprime la segunda posición de esta tupla.
+
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+print( 'La segunda posición de la tupla contiene el color:',colores[1] )
+
+# 34.- Utiliza los símbolos de suma y resta para obtener el resultado 25 a partir de los elementos de la siguiente tupla en una variable llamada operacion.
+
+numeros = (10, 1, 5, 11)
+print( numeros[0] - numeros[1] + numeros[2] + numeros[3]) 
