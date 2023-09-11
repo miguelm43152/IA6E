0a13c9977738c4eb807517d9ceaad1adef614103
miguelm43152 <sweetbloodmigue@gmail.com>
2023-09-10 18:29:02 -0600

    Create 21 IF y oprdrs de comparacion.py

---
+++ 21 IF y oprdrs de comparacion.py
@@ -0,0 +1,29 @@
+# Miguel Ángel Mendoza Hernández
+# Registro: 20110144
+# Inteligencia Artificial
+# Grupo 6E
+########### CAPITULO 21: El condicional IF y operadores de comparación  ##################################
+
+# 36 Cambia el operador para que la condición sea True.
+
+num1 = 15
+num2 = 20
+
+if num1 < num2:
+	print('Se ejecuta el if.')
+	
+# 37 Cambia el operador para que la condición sea True.
+
+num1 = 1450
+num2 = 60
+
+if num1 > num2:
+	print('Se ejecuta el if.')
+	
+# 38 Haz que el siguiente condicional se convierta en False sin cambiar el operador.
+
+num1 = 1450
+num2 = 60
+
+if num1 != num2:
+	print('Se ejecuta el if.')
