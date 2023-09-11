1392253ff9debd8fc90c745bfe53fe4e251727a3
miguelm43152 <sweetbloodmigue@gmail.com>
2023-09-10 19:28:11 -0600

    Create 26 tips para condicionales.py
    Ejecutar if en una líanea de código
    palabra pass para ejecutar if vacíos

---
+++ 26 tips para condicionales.py
@@ -0,0 +1,17 @@
+# Miguel Ángel Mendoza Hernández
+# Registro: 20110144
+# Inteligencia Artificial
+# Grupo 6E
+##################### CAPITULO 26: Tips para condicionales ###########################################
+
+### podemos incluir una sentencia if else en una única línea de la siguiente manera
+
+x = 100; y = 200
+
+print( "se ejecuta el if. ") if x < y else print('se ejecuta el else')
+
+#también podemos usar la palabra 'pass' para ejecutar if que
+#no tengan ningun contenido en ellos.
+
+if x < y:
+    pass
