9817c7f6191dbd645197abc296136328fdd7e5ac
miguelm43152 <sweetbloodmigue@gmail.com>
2023-09-10 18:29:00 -0600

    Create 22 el condicional IF ELSE.py

---
+++ 22 el condicional IF ELSE.py
@@ -0,0 +1,14 @@
+# Miguel Ángel Mendoza Hernández
+# Registro: 20110144
+# Inteligencia Artificial
+# Grupo 6E
+########### CAPITULO 22: El condicional IF ELSE  ##################################
+
+# 39 Corrige el siguiente condicional if else.
+
+color = "rojo"
+
+if color == "rojo":
+    print( "El color es rojo.")
+else:
+    print( "El color no es rojo.")
