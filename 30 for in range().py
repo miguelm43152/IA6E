4ad8584bb9cad473246a3b7203d956a830e4d282
miguelm43152 <sweetbloodmigue@gmail.com>
2023-09-10 20:27:40 -0600

    funcion range()

---
+++ 30 for in range().py
@@ -0,0 +1,13 @@
+# Miguel Ángel Mendoza Hernández
+# Registro: 20110144
+# Inteligencia Artificial
+# Grupo 6E
+############# CAPITULO 30: El bucle for II ######################
+
+# 47.- Crea un bucle for con un range() que vaya desde el valor 7 hasta el valor 700 en saltos de 100.
+#      Basta con que imprimas el valor de cada iteración.
+
+for x in range(7,700,100):
+    print(x)
+else:
+    print("Ha terminado el bucle for...")
