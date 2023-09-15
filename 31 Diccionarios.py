f58c8a177d9d1f927bc39ff8a3792ae37e6bd052
miguelm43152 <143959244+miguelm43152@users.noreply.github.com>
2023-09-14 18:48:38 -0600

    Crear diccionarios
    Y extraer su contenido

---
+++ 31 Diccionarios.py
@@ -0,0 +1,26 @@
+# Miguel Ángel Mendoza Hernández
+# Registro: 20110144
+# Inteligencia Artificial
+# Grupo: 6E
+############ CAPITULO 31: Diccionarios en Python ####################
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+print(teclado2)
+
+
+# 48.- Del diccionario teclado2 del capítulo, muestra los elementos Modelo
+#    y Precio con presentación en un print(). El resultado será esto en la
+#    consola:
+
+print( 'El modelo del teclado 2 es:', teclado2['Modelo'],
+       "y su precio es:", teclado2['Precio'])
