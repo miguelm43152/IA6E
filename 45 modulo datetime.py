d3cc5d3ed5db642e9355a875fcf106539f60268b
miguelm43152 <143959244+miguelm43152@users.noreply.github.com>
2023-09-16 18:29:41 -0600

    trabajar con fechas

---
+++ 45 modulo datetime.py
@@ -0,0 +1,15 @@
+# Miguel Ángel Mendoza Hernández
+# Registro: 20110144
+# Inteligencia Artificial
+# Grupo: 6E
+####### CAPITULO 45: Fechas con modulo datetime ####################
+
+import datetime
+
+fecha = datetime.datetime.now()
+
+print(fecha)
+
+fecha = datetime.datetime(2020, 9, 29, 19, 45)
+
+print(fecha)
